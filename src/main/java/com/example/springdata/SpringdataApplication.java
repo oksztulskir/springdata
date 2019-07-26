f8340ad2c0123e103c7846122d5be0727c914c37
rafal.oksztuski <rafal.oksztuski@pentacomp.pl>
2019-07-26 20:21:33 +0200

    Initial commit.

--- src/main/java/com/example/springdata/SpringdataApplication.java
+++ src/main/java/com/example/springdata/SpringdataApplication.java
@@ -1,34 +1,6 @@
 package com.example.springdata;
 
-import com.example.springdata.domain.model.User;
-import com.example.springdata.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.springdata.repository")
-public class SpringdataApplication implements CommandLineRunner {
-	private static final Logger LOGGER = LoggerFactory.getLogger(SpringdataApplication.class);
-
-	@Autowired
-	private UserService userService;
-
+public class SpringdataApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdataApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-		final String username = "kowalskij";
-		LOGGER.info("Getting user for username={}", username);
-		final User foundUser = userService.findByUsername(username);
-		LOGGER.info("Found user: {}", foundUser);
-		final User userForAdding = new User("nowaka", "Andrzej", "Nowak");
-		LOGGER.info("User added: {}", userService.save(userForAdding));
 	}
 }
