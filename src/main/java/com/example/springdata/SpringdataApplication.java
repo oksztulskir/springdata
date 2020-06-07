e34b2283adee69533b982ea820e056b1e5a4ab97
rafal.oksztuski <rafal.oksztuski@pentacomp.pl>
2020-06-07 18:54:36 +0200

    UserRepository Added.

--- src/main/java/com/example/springdata/SpringdataApplication.java
+++ src/main/java/com/example/springdata/SpringdataApplication.java
@@ -1,6 +1,41 @@
 package com.example.springdata;
 
-public class SpringdataApplication {
+
+import com.example.springdata.domain.model.User;
+import com.example.springdata.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringdataApplication.class);
+
 	public static void main(String[] args) {
+		SpringApplication.run(SpringdataApplication.class, args);
+	}
+
+	private UserRepository userRepository;
+
+	@Autowired
+	public SpringdataApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("Spring Boot Application started successfuly!");
+		LOGGER.trace("Trace: Spring Boot Application started successfuly!");
+
+		User user1 = new User("Kowalski", "jkowalski");
+		User user2 = new User("Nowak", "anowak");
+
+		userRepository.save(user1);
+		userRepository.save(user2);
+
+		System.out.println(userRepository.findByLogin("jkowalski"));
 	}
 }
--- src/main/java/com/example/springdata/domain/model/User.java
+++ src/main/java/com/example/springdata/domain/model/User.java
@@ -1,5 +1,56 @@
 package com.example.springdata.domain.model;
 
-public class User {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String login;
+
+    public User() {
+    }
+
+    public User(String name, String login) {
+        this.name = name;
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", login='" + login + '\'' +
+                '}';
+    }
 }
